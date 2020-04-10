2ee0832603b98dcf0327f3cb03e627046a97a9d6
Ian <ianpc122@gmail.com>
2020-04-10 20:37:39 +0100

    side dish of stuffing

--- server/src/main/java/com/example/codeclan/itemservice/components/DataLoader.java
+++ server/src/main/java/com/example/codeclan/itemservice/components/DataLoader.java
@@ -31,15 +31,40 @@ public class DataLoader implements ApplicationRunner {
         Category freezer = new Category("Freezer");
         categoryRepository.save(freezer);
 
+        Category cupboard = new Category("Cupboard");
+        categoryRepository.save(cupboard);
+
         FoodItem milk = new FoodItem("Milk",3, fridge);
         foodItemRepository.save(milk);
 
         FoodItem lemonade = new FoodItem("Lemonade", 1, fridge);
         foodItemRepository.save(lemonade);
 
+        FoodItem beans = new FoodItem("Beans", 2, cupboard);
+        foodItemRepository.save(beans);
+
+        FoodItem soup = new FoodItem("Soup",2, cupboard);
+        foodItemRepository.save(soup);
+
+        FoodItem bread = new FoodItem("Bread", 1, cupboard);
+        foodItemRepository.save(bread);
+
+        FoodItem yoghurt = new FoodItem("Yoghurt", 1, fridge);
+        foodItemRepository.save(yoghurt);
+
+        FoodItem cheese = new FoodItem("Cheese", 1, fridge);
+        foodItemRepository.save(cheese);
+
+        FoodItem icecream = new FoodItem("Ice Cream", 1, freezer);
+        foodItemRepository.save(icecream);
+
+        FoodItem peas = new FoodItem("peas", 2, freezer);
+        foodItemRepository.save(peas);
+
         FoodItem chips = new FoodItem("Chips", 4, freezer);
         chips.putOnShoppingList();
         foodItemRepository.save(chips);
+
 
 
     }
